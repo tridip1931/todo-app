6c92bbef7823a76e4f23052007795eba9e33e7ea
Tridip Thrizu <tridip@mapbox.com>
2017-08-29 14:59:06 +0530

    Change title, pull in assembly and clean up index.js

--- src/index.js
+++ src/index.js
@@ -1,8 +1,5 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
-import registerServiceWorker from './registerServiceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
-registerServiceWorker();
+ReactDOM.render(<h1>TODO APP</h1>, document.getElementById('root'));
