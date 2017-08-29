4eb6a7ec3a563073f9618a6e906215ed55d0e125
Tridip Thrizu <tridip@mapbox.com>
2017-08-29 15:04:06 +0530

    Add TODOS data

--- src/index.js
+++ src/index.js
@@ -2,4 +2,13 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 
+
+const TODOS = [
+  { id: 1503999071258, text: "Help Dave with Project", completed: false },
+  { id: 1503999071268, text: "Make important phone call", completed: false },
+  { id: 1503999071278, text: "Email Jason", completed: false },
+  { id: 1503999071288, text: "Drop by mail center", completed: true },
+  { id: 1503999071298, text: "Pay credit card bill", completed: true },
+];
+
 ReactDOM.render(<h1>TODO APP</h1>, document.getElementById('root'));
